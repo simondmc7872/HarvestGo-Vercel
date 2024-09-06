27d0cd5f36bc39626f2ae4f6fb5167fb817d40b3
Simon Chan <Simondmc@gmail.com>
2024-09-05 15:40:44 -1000

    Update CORS config in server.js

--- server.js
+++ server.js
@@ -19,7 +19,11 @@ const pool = new Pool({
 })
 
 app.use("/images", express.static(path.join(__dirname, "public/images")))
-app.use(cors())
+app.use(
+  cors({
+    origin: "https://harvest-go-vercel.vercel.app",
+  })
+)
 
 app.get("/api/meatseafoodavail", async (req, res) => {
   try {
