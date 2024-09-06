05c880f548ff9dc08f4dfc3267b1bbb410300fde
Simon Chan <Simondmc@gmail.com>
2024-09-05 16:40:22 -1000

    Updated verce.json /meatseafoodavail, no /api

--- selectTables.sql
+++
@@ -1,4 +0,0 @@
-      SELECT * FROM Products
-      INNER JOIN Main_Warehouse ON Products.itemid = Main_Warehouse.itemid
-      INNER JOIN Suppliers ON Main_Warehouse.supid = Suppliers.supid
-      
