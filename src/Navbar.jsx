822cdbda97271352ea7db848d406466dd0ba7290
Simon Chan <Simondmc@gmail.com>
2024-10-26 05:54:28 -1000

    added .btn-Media

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,50 +7,55 @@ export default function Navbar(props) {
   )
 
   return (
-    <nav className="navbar">
-      <NavLink to="/app" className="nav-brand">
-        HarvestGO
-      </NavLink>
-      <ul className="navbarMedia">
-        <li className="nav-item">
-          <NavLink
-            className={({ isActive }) => (isActive ? "active" : "")}
-            to="/app"
-            end
-          >
-            Home
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink
-            className={({ isActive }) => (isActive ? "active" : "")}
-            to="/app/about"
-          >
-            About us
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink
-            className={({ isActive }) => (isActive ? "active" : "")}
-            to="/app/products"
-          >
-            Products
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink
-            className={({ isActive }) => (isActive ? "active" : "")}
-            to="/app/meatseafoodavail"
-          >
-            Meat & Seafood Availability
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/app/cart" className="nav-item nav-cart btn btn-accent">
-            Cart ({cartCount})
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <div className="containerMedia">
+      <nav className="navbar">
+        <NavLink to="/app" className="nav-brand">
+          HarvestGO
+        </NavLink>
+        <ul className="navbarMedia">
+          <li className="nav-item">
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : "")}
+              to="/app"
+              end
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : "")}
+              to="/app/about"
+            >
+              About us
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : "")}
+              to="/app/products"
+            >
+              Products
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : "")}
+              to="/app/meatseafoodavail"
+            >
+              Meat & Seafood Availability
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/app/cart"
+              className="nav-item nav-cart btn btn-accent btn-Media"
+            >
+              Cart ({cartCount})
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 }
