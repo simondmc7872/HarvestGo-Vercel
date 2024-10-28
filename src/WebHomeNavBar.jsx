441dce8c17adb7c79e2db7b450348bf967cccd53
Simon Chan <Simondmc@gmail.com>
2024-10-28 09:04:06 -1000

    comprehensive adj to media css/move media to bottom desktopfirst approach. adj to webhomenavbar.jsx. still need to fix webhomenavbar and webhome

--- src/WebHomeNavBar.jsx
+++ src/WebHomeNavBar.jsx
@@ -2,45 +2,47 @@ import { NavLink } from "react-router-dom"
 
 function WebHomeNavbar() {
   return (
-    <nav className="navbarM">
-      <NavLink to="/" className="nav-brandM">
-        Simon Chan
-      </NavLink>
-      <ul>
-        <li className="nav-itemM">
-          <NavLink
-            className={({ isActive }) => (isActive ? "active" : "")}
-            to="/"
-          >
-            Home
-          </NavLink>
-        </li>
-        <li className="nav-itemM">
-          <NavLink
-            className={({ isActive }) => (isActive ? "active" : "")}
-            to="webhomeabout"
-          >
-            About
-          </NavLink>
-        </li>
-        <li className="nav-itemM">
-          <NavLink
-            className={({ isActive }) => (isActive ? "active" : "")}
-            to="webhomeapp"
-          >
-            Project
-          </NavLink>
-        </li>
-        <li className="nav-itemM">
-          <NavLink
-            className={({ isActive }) => (isActive ? "active" : "")}
-            to="contact"
-          >
-            Contact
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <div className="webcontainermedia">
+      <nav className="navbarM">
+        <NavLink to="/" className="nav-brandM">
+          Simon Chan
+        </NavLink>
+        <ul className="webNavbarMedia">
+          <li className="nav-itemM">
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : "")}
+              to="/"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-itemM">
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : "")}
+              to="webhomeabout"
+            >
+              About
+            </NavLink>
+          </li>
+          <li className="nav-itemM">
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : "")}
+              to="webhomeapp"
+            >
+              Project
+            </NavLink>
+          </li>
+          <li className="nav-itemM">
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : "")}
+              to="contact"
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
