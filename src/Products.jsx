cf791d35be7d1b1a0af564924e0a5d7a6041e65f
Simon Chan <Simondmc@gmail.com>
2024-11-05 13:56:07 -1000

    media css adj for About Us and Products page. Products half finished

--- src/Products.jsx
+++ src/Products.jsx
@@ -20,7 +20,7 @@ export default function Products(props) {
   return (
     <div className="products-layout">
       <h1>Products</h1>
-      <p>Take a look at our products</p>
+      <p className="center-desc">Take a look at our products</p>
       <div className="products-grid">
         {loading && <Loader />}
         {products.map((product) => {
